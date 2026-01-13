4d6dbe2bb6562d8d031be26fb03a6c725218080b
rakeshparveen5 <rakesh.parveen5@gmail.com>
2026-01-13 08:20:12 +0530

    Handles new lines in numbers: Passed test case

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,7 +2,7 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final parts = numbers.split(',');
+    final parts = numbers.split(RegExp('[,\n]'));
     return parts.map(int.parse).reduce((a, b) => a + b);
   }
 }
