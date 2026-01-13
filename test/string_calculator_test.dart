51d7a12d88714dc8d54838be374298066be3f816
rakeshparveen5 <rakesh.parveen5@gmail.com>
2026-01-13 07:55:08 +0530

    Empty string returns 0 - failed test case

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,3 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  // TODO: Add StringCalculator Unit Tests
+  late StringCalculator calculator;
+
+  setUp(() {
+    calculator = StringCalculator();
+  });
+
+  group('test add', () {
+    test('returns 0 for empty string', () {
+      expect(calculator.add(''), equals(0));
+    });
+  });
 }
