335d97c77568f27c9823dffae47bf31d8745ac5e
rakeshparveen5 <rakesh.parveen5@gmail.com>
2026-01-13 08:29:26 +0530

    Handles other delimiters in numbers-Pass test case

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,7 +2,10 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final parts = numbers.split(RegExp('[,\n]'));
-    return parts.map(int.parse).reduce((a, b) => a + b);
+    final parts = numbers.split(RegExp('[,\n;//]'));
+    return parts
+        .where((v) => v.isNotEmpty)
+        .map(int.parse)
+        .reduce((a, b) => a + b);
   }
 }
