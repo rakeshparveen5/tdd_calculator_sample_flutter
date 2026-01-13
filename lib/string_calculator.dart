c41ce48feb02a0693665cb7f4967d9d1d0a2b295
rakeshparveen5 <rakesh.parveen5@gmail.com>
2026-01-13 08:48:03 +0530

    Handle -ve number with exception- Passed test case

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,15 @@ class StringCalculator {
     if (numbers.isEmpty) return 0;
 
     final parts = numbers.split(RegExp('[,\n;//]'));
+    final negativeNumbers = parts.where(
+      (v) => (int.tryParse(v) ?? 0).isNegative,
+    );
+    if (negativeNumbers.isNotEmpty) {
+      throw Exception(
+        'negative numbers not allowed ${negativeNumbers.join(',')}',
+      );
+    }
+
     return parts
         .where((v) => v.isNotEmpty)
         .map(int.parse)
